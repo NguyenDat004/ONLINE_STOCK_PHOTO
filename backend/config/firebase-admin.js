c783f78a8d4b0134120d183fb243888338a54494
NguyenDat004 <nguyendat135ok@gmail.com>
2026-04-07 19:41:27 +0700

    Fix Firebase Admin for deployment

--- backend/config/firebase-admin.js
+++ backend/config/firebase-admin.js
@@ -1,11 +1,26 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebaseServiceAccountKey.json');
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./firebaseServiceAccountKey.json');
 
-// Tránh lỗi khi khởi tạo lại app nhiều lần
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+// // Tránh lỗi khi khởi tạo lại app nhiều lần
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
+
+// module.exports = admin;
+
+
+const admin = require("firebase-admin");
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 module.exports = admin;
