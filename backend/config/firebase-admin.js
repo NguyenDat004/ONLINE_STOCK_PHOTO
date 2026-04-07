e4dd41dbfe7075ed3e3d6f2b9d8a408133046411
NguyenDat004 <nguyendat135ok@gmail.com>
2026-04-07 20:01:29 +0700

    Fix .env for deployment

--- backend/config/firebase-admin.js
+++ backend/config/firebase-admin.js
@@ -1,26 +1,23 @@
-// const admin = require('firebase-admin');
-// const serviceAccount = require('./firebaseServiceAccountKey.json');
-
-// // Tránh lỗi khi khởi tạo lại app nhiều lần
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//   });
-// }
-
-// module.exports = admin;
-
-
 const admin = require("firebase-admin");
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-};
+let serviceAccount;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (process.env.NODE_ENV === "production") {
+  // deploy trên Render: lấy từ env
+  serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  };
+} else {
+  // local: dùng file JSON
+  serviceAccount = require("./firebaseServiceAccountKey.json");
+}
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
